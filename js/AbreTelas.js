d3f06527cef4de7c0db5850007097fa3567d1056
Lucas Barauna <82176900+LucasBara-dising@users.noreply.github.com>
2023-11-08 23:27:45 -0300

    Tela de feed do filtro

---
+++ js/AbreTelas.js
@@ -0,0 +1,11 @@
+function Abrefeed(){
+    window.location.href= "Feed.html"
+}
+
+function AbrePerfil(){
+    window.location.href= "indexPageUser.html"
+}
+
+function AbrePerfil(){
+    window.location.href= "indexPageUser.html"
+}
