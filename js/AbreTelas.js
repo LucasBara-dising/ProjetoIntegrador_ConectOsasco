b43524e5a413fe1aa60c2c3b51733e86d0e7e6e2
Lucas Barauna <82176900+LucasBara-dising@users.noreply.github.com>
2023-11-13 20:44:53 -0300

    Telas de new post e filtro

--- js/AbreTelas.js
+++ js/AbreTelas.js
@@ -6,6 +6,14 @@ function AbrePerfil(){
     window.location.href= "indexPageUser.html"
 }
 
-function AbrePerfil(){
-    window.location.href= "indexPageUser.html"
+function AbreFiltro(){
+    window.location.href= "Filtro.html"
+}
+
+function AbreFeedFiltro(){
+    window.location.href= "FeedFiltro.html"
+}
+
+function AbreNewPost(){
+    window.location.href= "NewPost.html"
 }
